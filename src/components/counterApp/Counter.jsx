475e46036132f52184213b057800661f55db1329
diwakar491 <diwakarkaran491@outlook.com>
2024-10-31 00:30:07 +0530

    Add increment

--- src/components/counterApp/Counter.jsx
+++ src/components/counterApp/Counter.jsx
@@ -1,8 +1,18 @@
 export default function Counter(){
 
+    function incrementCounterFunction(){
+        console.log('increment clicked')
+    }
+
+
     return(
         <div className="Counter">
-            Counter
+            
+            <span className="count">0</span>
+            <div>
+            <button className="counterButton" onClick={incrementCounterFunction}>+1</button>
             </div>
+
+        </div>
     )
 }
